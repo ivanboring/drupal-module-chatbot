ad407c5da7988b3f87ead695979915cbf8b935e6
Marcus Johansson <me@marcusmailbox.com>
2025-06-19 15:02:24 +0200

    Add the pgvector apt repo

--- .devpanel/setup_postgres.sh
+++ .devpanel/setup_postgres.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# Prepare so it works in devpanel also.
+sudo apt -y install curl ca-certificates
+sudo install -d /usr/share/postgresql-common/pgdg
+sudo curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://www.postgresql.org/media/keys/ACCC4CF8.asc
+. /etc/os-release
+sudo sh -c "echo 'deb [signed-by=/usr/share/postgresql-common/pgdg/apt.postgresql.org.asc] https://apt.postgresql.org/pub/repos/apt $VERSION_CODENAME-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
+sudo apt-get update
+
 #== Install postgresql on the host.
 echo 'PostgreSQL is not installed. Installing it now.'
 time sudo apt-get update
